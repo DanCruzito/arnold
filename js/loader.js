ff4367c6a891fc9a5f3d40ea0aa3379f500d51fa
DanCruzito <dancruzito@gmail.com>
2020-08-28 18:28:47 -0400

    pruebaa

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',()=>{
+    const conetendor_loader = document.querySelector('.contenedor_loader');
+    conetendor_loader.style.opacity = 0
+    conetendor_loader.style.visibility = 'hidden'
+})
---
+++ js/menu.js
@@ -0,0 +1,3 @@
+document.querySelector(".plus-btn").addEventListener("click", function(){
+    document.querySelector("body").classList.toggle("menu-open");
+  })
